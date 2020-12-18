a637d108ab6ab849081c3bdc5d2d8231e12101a2
Mehmet Can Taş <tass.mehmetcan@outlook.com>
2020-12-18 22:27:50 +0300

    Add LaunchRover method to RoverService

--- HbMarsRover/Services/IRoverService.cs
+++ HbMarsRover/Services/IRoverService.cs
@@ -6,6 +6,7 @@ namespace HbMarsRover.Services
         Rover TurnRight(Rover rover);
         Rover MoveForward(Rover rover,Plateau plateau);
 
+        Rover LaunchRover(Rover rover,Plateau plateau,string coordinates);
         Rover MoveToLocation(Rover rover,Plateau plateau,string commands);
     }
 }
