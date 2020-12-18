018281c86854f95938fa75ed4c9973a2e0116ee3
Mehmet Can Taş <tass.mehmetcan@outlook.com>
2020-12-18 21:05:50 +0300

    Create PlateauTests.cs

---
+++ HbMarsRoverUnitTest/PlateauTests.cs
@@ -0,0 +1,48 @@
+using System;
+using FluentAssertions;
+using HbMarsRover;
+using Xunit;
+
+namespace HbMarsRoverUnitTest
+{
+    public class PlateauTests
+    {
+        [Theory]
+        [InlineData(3,3)]
+        [InlineData(4,4)]
+        [InlineData(5,5)]
+        [InlineData(6,6)]
+        public void Should_Success_Create_Plateau_When_Given_Valid_Parameters(int width,int height)
+        {
+            Plateau plateau = new();
+            
+            plateau.SetHeight(height);
+            plateau.SetWidth(width);
+            
+            
+            
+            plateau.Should().NotBeNull();
+            plateau.Width.Should().Be(width);
+            plateau.Height.Should().Be(height);
+        }
+        
+        [Theory]
+        [InlineData(4,0)]
+        [InlineData(5,0)]
+        public void Should_Fail_Create_Plateau_When_Given_Invalid_Height(int width,int height)
+        {
+            Plateau plateau = new();
+            Assert.Throws<ArgumentException>(() => plateau.SetHeight(height));
+        }
+        
+        [Theory]
+        [InlineData(0,3)]
+        [InlineData(0,4)]
+        public void Should_Fail_Create_Plateau_When_Given_Invalid_Width(int width,int height)
+        {
+            Plateau plateau = new();
+            Assert.Throws<ArgumentException>(() => plateau.SetWidth(width));
+
+        }
+    }
+}
