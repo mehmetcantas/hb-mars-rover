9f87f300c188ee132507d00786c26ecf2401b4ca
Mehmet Can Taş <tass.mehmetcan@outlook.com>
2020-12-18 22:28:02 +0300

    Add movement ability check

--- HbMarsRover/Services/RoverService.cs
+++ HbMarsRover/Services/RoverService.cs
@@ -63,18 +63,28 @@ namespace HbMarsRover.Services
             {
                 
                 case "N": // While you facing North and if you turn right you facing to East
-                        rover.YCoordinate += 1;
+                    if ((rover.YCoordinate + 1) > plateau.Width)
+                        throw new Exception("Rover cannot be move any further on the Y axis");
+                    
+                    rover.YCoordinate += 1;
                     break;
                 
                 case "S": // While you facing South and if you turn right you facing to West
+                    if ((rover.YCoordinate - 1) < 0)
+                        throw new Exception("Rover cannot be move any further on Y axis");
+                    
                     rover.YCoordinate -= 1;
                     break;
                 
                 case "W": // While you facing West and if you turn right you facing to North
+                    if ((rover.XCoordinate - 1) < 0)
+                        throw new Exception("Rover cannot be move any further on the X axis");
                     rover.XCoordinate -= 1;
                     break;
                
                 case "E":  // While you facing East and if you turn right you facing to South
+                    if ((rover.XCoordinate + 1) > plateau.Width)
+                        throw new Exception("Rover cannot be move any further on the X axis");
                     rover.XCoordinate += 1;
                     break;
                 default:
@@ -84,6 +94,23 @@ namespace HbMarsRover.Services
             return rover;
         }
 
+        public Rover LaunchRover(Rover rover,Plateau plateau,string coordinates)
+        {
+            var launchLocation = coordinates.Split(" ");
+
+            rover.XCoordinate = int.Parse(launchLocation[0]);
+            rover.YCoordinate = int.Parse(launchLocation[1]);
+            rover.Direction = launchLocation[2];
+
+            if (rover.YCoordinate > plateau.Width || rover.YCoordinate < 1)
+                throw new Exception("Rover cannot be launched on this coordinates");
+            
+            if (rover.XCoordinate > plateau.Height || rover.XCoordinate < 1)
+                throw new Exception("Rover cannot be launched on this coordinates");
+            
+            
+            return rover;
+        }
 
         public Rover MoveToLocation(Rover rover ,Plateau plateau, string commands)
         {
