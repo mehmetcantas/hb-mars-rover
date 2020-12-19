af98716afc1bc8b6b40d0f08017ae73a64844de5
Mehmet Can Taş <tass.mehmetcan@outlook.com>
2020-12-19 23:03:20 +0300

    Move RoverService class into the Rover class

--- HbMarsRover/Models/Rover.cs
+++ HbMarsRover/Models/Rover.cs
@@ -1,3 +1,4 @@
+using System;
 using HbMarsRover.Enums;
 
 namespace HbMarsRover
@@ -12,8 +13,127 @@ namespace HbMarsRover
     
     public class Rover
     {
-        public int XCoordinate { get; set; }
-        public int YCoordinate { get; set; }
-        public Direction Direction { get; set; }
+        public int XCoordinate { get; private set; }
+        public int YCoordinate { get; private set; }
+        public Direction Direction { get; private set; }
+        
+        public void TurnLeft()
+        {
+            switch (Direction)
+            {
+                case Direction.N: // While you facing North and if you turn left you facing to West
+                    Direction = Direction.W;
+                    break;
+
+                case Direction.S: // While you facing South and if you turn left you facing to East
+                    Direction = Direction.E;
+                    break;
+
+                case Direction.W: // While you facing West and if you turn left you facing to South
+                    Direction = Direction.S;
+                    break;
+
+                case Direction.E: // While you facing East and if you turn left you facing to North
+                    Direction = Direction.N;
+                    break;
+                default:
+                    throw new ArgumentException("Direction is invalid");
+            }
+        }
+
+        public void TurnRight()
+        {
+            switch (Direction)
+            {
+                case Direction.N: // While you facing North and if you turn right you facing to East
+                    Direction = Direction.E;
+                    break;
+
+                case Direction.S: // While you facing South and if you turn right you facing to West
+                    Direction = Direction.W;
+                    break;
+
+                case Direction.W: // While you facing West and if you turn right you facing to North
+                    Direction = Direction.N;
+                    break;
+
+                case Direction.E: // While you facing East and if you turn right you facing to South
+                    Direction = Direction.S;
+                    break;
+                default:
+                    throw new ArgumentException("Direction is invalid");
+            }
+        }
+
+        public void MoveForward(Plateau plateau)
+        {
+            switch (Direction)
+            {
+                case Direction.N: // While you facing North and if you turn right you facing to East
+                    if ((YCoordinate + 1) > plateau.Width)
+                        throw new Exception("Rover cannot be move any further on the Y axis");
+
+                    YCoordinate += 1;
+                    break;
+
+                case Direction.S: // While you facing South and if you turn right you facing to West
+                    if ((YCoordinate - 1) < 0)
+                        throw new Exception("Rover cannot be move any further on Y axis");
+
+                    YCoordinate -= 1;
+                    break;
+
+                case Direction.W: // While you facing West and if you turn right you facing to North
+                    if ((XCoordinate - 1) < 0)
+                        throw new Exception("Rover cannot be move any further on the X axis");
+                    XCoordinate -= 1;
+                    break;
+
+                case Direction.E: // While you facing East and if you turn right you facing to South
+                    if ((XCoordinate + 1) > plateau.Width)
+                        throw new Exception("Rover cannot be move any further on the X axis");
+                    XCoordinate += 1;
+                    break;
+                default:
+                    throw new ArgumentException("Direction is invalid");
+            }
+        }
+
+        public void LaunchRover(Plateau plateau, string coordinates)
+        {
+            var launchLocation = coordinates.Split(" ");
+
+            XCoordinate = int.Parse(launchLocation[0]);
+            YCoordinate = int.Parse(launchLocation[1]);
+            Direction = (Direction) Enum.Parse(typeof(Direction), launchLocation[2].ToUpper());
+
+            if (YCoordinate > plateau.Width || YCoordinate < 0)
+                throw new Exception("Rover cannot be launched on this coordinates");
+
+            if (XCoordinate > plateau.Height || XCoordinate < 0)
+                throw new Exception("Rover cannot be launched on this coordinates");
+        }
+
+        public void MoveToLocation(Plateau plateau, string commands)
+        {
+            var seperatedCommands = commands.ToCharArray();
+            foreach (var item in seperatedCommands)
+            {
+                switch (item)
+                {
+                    case 'L':
+                        TurnLeft();
+                        break;
+                    case 'R':
+                        TurnRight();
+                        break;
+                    case 'M':
+                        MoveForward(plateau);
+                        break;
+                    default:
+                        throw new ArgumentException("Location command unrecognized");
+                }
+            }
+        }
     }
 }
