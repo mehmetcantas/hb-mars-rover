05011b943ce16619066ee2bf4f20fce9fb54584f
Mehmet Can Taş <tass.mehmetcan@outlook.com>
2020-12-19 15:46:10 +0300

    Lint RoverService class

--- HbMarsRover/Services/RoverService.cs
+++ HbMarsRover/Services/RoverService.cs
@@ -12,15 +12,15 @@ namespace HbMarsRover.Services
                 case Direction.N: // While you facing North and if you turn left you facing to West
                     rover.Direction = Direction.W;
                     break;
-                
+
                 case Direction.S: // While you facing South and if you turn left you facing to East
                     rover.Direction = Direction.E;
                     break;
-                
+
                 case Direction.W: // While you facing West and if you turn left you facing to South
                     rover.Direction = Direction.S;
                     break;
-                
+
                 case Direction.E: // While you facing East and if you turn left you facing to North
                     rover.Direction = Direction.N;
                     break;
@@ -35,20 +35,19 @@ namespace HbMarsRover.Services
         {
             switch (rover.Direction)
             {
-                
                 case Direction.N: // While you facing North and if you turn right you facing to East
                     rover.Direction = Direction.E;
                     break;
-                
+
                 case Direction.S: // While you facing South and if you turn right you facing to West
                     rover.Direction = Direction.W;
                     break;
-                
+
                 case Direction.W: // While you facing West and if you turn right you facing to North
                     rover.Direction = Direction.N;
                     break;
-               
-                case Direction.E:  // While you facing East and if you turn right you facing to South
+
+                case Direction.E: // While you facing East and if you turn right you facing to South
                     rover.Direction = Direction.S;
                     break;
                 default:
@@ -58,32 +57,31 @@ namespace HbMarsRover.Services
             return rover;
         }
 
-        public Rover MoveForward(Rover rover,Plateau plateau)
+        public Rover MoveForward(Rover rover, Plateau plateau)
         {
             switch (rover.Direction)
             {
-                
                 case Direction.N: // While you facing North and if you turn right you facing to East
                     if ((rover.YCoordinate + 1) > plateau.Width)
                         throw new Exception("Rover cannot be move any further on the Y axis");
-                    
+
                     rover.YCoordinate += 1;
                     break;
-                
+
                 case Direction.S: // While you facing South and if you turn right you facing to West
                     if ((rover.YCoordinate - 1) < 0)
                         throw new Exception("Rover cannot be move any further on Y axis");
-                    
+
                     rover.YCoordinate -= 1;
                     break;
-                
+
                 case Direction.W: // While you facing West and if you turn right you facing to North
                     if ((rover.XCoordinate - 1) < 0)
                         throw new Exception("Rover cannot be move any further on the X axis");
                     rover.XCoordinate -= 1;
                     break;
-               
-                case Direction.E:  // While you facing East and if you turn right you facing to South
+
+                case Direction.E: // While you facing East and if you turn right you facing to South
                     if ((rover.XCoordinate + 1) > plateau.Width)
                         throw new Exception("Rover cannot be move any further on the X axis");
                     rover.XCoordinate += 1;
@@ -95,25 +93,25 @@ namespace HbMarsRover.Services
             return rover;
         }
 
-        public Rover LaunchRover(Rover rover,Plateau plateau,string coordinates)
+        public Rover LaunchRover(Rover rover, Plateau plateau, string coordinates)
         {
             var launchLocation = coordinates.Split(" ");
 
             rover.XCoordinate = int.Parse(launchLocation[0]);
             rover.YCoordinate = int.Parse(launchLocation[1]);
-            rover.Direction = (Direction)Enum.Parse(typeof(Direction),launchLocation[2].ToUpper());
+            rover.Direction = (Direction) Enum.Parse(typeof(Direction), launchLocation[2].ToUpper());
 
             if (rover.YCoordinate > plateau.Width || rover.YCoordinate < 0)
                 throw new Exception("Rover cannot be launched on this coordinates");
-            
+
             if (rover.XCoordinate > plateau.Height || rover.XCoordinate < 0)
                 throw new Exception("Rover cannot be launched on this coordinates");
-            
-            
+
+
             return rover;
         }
 
-        public Rover MoveToLocation(Rover rover ,Plateau plateau, string commands)
+        public Rover MoveToLocation(Rover rover, Plateau plateau, string commands)
         {
             var seperatedCommands = commands.ToCharArray();
             foreach (var item in seperatedCommands)
@@ -127,7 +125,7 @@ namespace HbMarsRover.Services
                         TurnRight(rover);
                         break;
                     case 'M':
-                        MoveForward(rover,plateau);
+                        MoveForward(rover, plateau);
                         break;
                     default:
                         throw new ArgumentException("Location command unrecognized");
